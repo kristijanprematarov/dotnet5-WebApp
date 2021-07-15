d739eb159b6c7742c52347a87b3e09b8da08c88c
kristijanprematarov <kristijanprematarov@yahoo.com>
2021-07-15 12:25:23 +0200

    initial

--- dotnet5-WebApp/Controllers/WeatherForecastController.cs
+++ dotnet5-WebApp/Controllers/WeatherForecastController.cs
@@ -1,12 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace dotnet5_WebApp.Controllers
+﻿namespace dotnet5_WebApp.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
