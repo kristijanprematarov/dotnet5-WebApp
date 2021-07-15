a798f9a66f7420f424fe40d5b5c51619100af973
kristijanprematarov <kristijanprematarov@yahoo.com>
2021-07-15 12:33:52 +0200

    Your first model

---
+++ dotnet5-WebApp/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet5_WebApp.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public string Place { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public bool IsDeleted { get; set; } = false;
+    }
+}
